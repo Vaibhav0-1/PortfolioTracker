6e979483854d27eed97d685035f6a7d638bbb361
Vaibhav0-1 <130361641+Vaibhav0-1@users.noreply.github.com>
2024-06-16 23:58:05 +0530

    Fetching Assets Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [assets, setAssets] = useState([]);
+  const [address, setAddress] = useState("");
+
+  const fetchAssets = async() =>{
+    try{
+      const response = await fetch('https://deep-index.moralis.io/api/v2.2/wallets/0x3d0b45Bc914457E027094E509eBE631E356cbB03/tokens?chain=eth&exclude_spam=true&exclude_unverified_contracts=true',{
+        method:"GET",
+        headers: {
+          "Content-Type":"applocation/json",
+          "X-API-Key": import.meta.env.REACT_APP_MORALIS_API_KEY
+        }
+      })
+      const data  = await response.json();
+      setAssets(data.result)
+      }catch(error){
+        console.log("Error Fetching Assets", error);
+    }
+  }
+
+  useEffect(()=>{
+    fetchAssets()
+  },[])
 
   return (
     <>

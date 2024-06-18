a820469fb8e7df98093a295489d1dcbe505b0226
Vaibhav0-1 <130361641+Vaibhav0-1@users.noreply.github.com>
2024-06-18 16:15:34 +0530

    Building Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import './App.css'
 
 function App() {
   const [assets, setAssets] = useState([]);
-  const [address, setAddress] = useState("");
+  const [address, setAddress] = useState('0x209c8bbE2454257Eb1A8E630f59f4b1b50a98543');
 
   const fetchAssets = async() =>{
     try{
-      const response = await fetch('https://deep-index.moralis.io/api/v2.2/wallets/0x3d0b45Bc914457E027094E509eBE631E356cbB03/tokens?chain=eth&exclude_spam=true&exclude_unverified_contracts=true',{
-        method:"GET",
+      const response = await fetch(
+        `https://deep-index.moralis.io/api/v2.2/wallets/${address}/tokens?chain=eth&exclude_spam=true&exclude_unverified_contracts=true`,
+        {
+        method:'GET',
         headers: {
           "Content-Type":"application/json",
-          "X-API-Key": import.meta.env.REACT_APP_MORALIS_API_KEY
-        }
-      })
+          "X-API-Key": import.meta.env.VITE_MORALIS_API_KEY
+        },
+      });
       const data  = await response.json();
       setAssets(data.result)
       }catch(error){
@@ -22,8 +24,8 @@ function App() {
   }
 
   useEffect(()=>{
-    fetchAssets()
-  },[])
+    fetchAssets(address);
+  },[address]);
 
   return (
       <div className='App'>
@@ -39,20 +41,32 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            {assets.map((asset)=>{
+            
+          {assets.length > 0 ? (
+            assets.map((asset) => (
               <tr key={asset.token_address}>
-                <td><img src={asset.thumbnail} alt={asset.name} className='asset-logo'/></td>
+                <td>
+                  <img
+                    src={asset.thumbnail}
+                    alt={asset.name}
+                    className="asset-logo"
+                  />
+                </td>
                 <td>{asset.name}</td>
                 <td>{asset.usd_price}</td>
                 <td>{asset.usd_value}</td>
                 <td>{asset.usd_price_24hr_percent_change}</td>
               </tr>
-            })}
+            ))
+          ) : (
+            <tr>
+              <td colSpan="5">No assets available</td>
+            </tr>
+          )}
           </tbody>
         </table>
-      
       </div>
-  )
-}
+  );
+};
 
 export default App

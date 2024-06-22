f4b827f7521a5744178cd92d41b40011f6ea114f
Vaibhav0-1 <130361641+Vaibhav0-1@users.noreply.github.com>
2024-06-22 23:50:45 +0530

    Adding Changes

--- src/Networth.jsx
+++ src/Networth.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 function Networth({ netWorth }) {
+
+  let USDollar = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
   return (
     <div>
-        <h1>{ netWorth.total_networth_usd }</h1>
+        <h1>{ USDollar.format(netWorth.total_networth_usd) }</h1>
       
     </div>
   )
